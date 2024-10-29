57c50a1f4d3c83de357f292097159f3d9a9ef770
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2024-10-29 14:13:22 -0600

    v2.0.1 - actually useful FULL stats, results_extra.txt

--- ML.py
+++ ML.py
@@ -1,4 +1,4 @@
-# ML 2.0.0 - AI simplified in 1 file, 80 lines. Verify that your model can generalize on the given training-            Run it: "apt install geany python3-torch". Open the .py in Geany.
+# ML 2.0.1 - AI simplified in 1 file, 80 lines. Verify that your model can generalize on the given training-            Run it: "apt install geany python3-torch". Open the .py in Geany.
 #            data by scoring well. Then replace the data with your own (label & string per line in text file.)          Replace "python" with "python3" in Geany's execute command. F5 to run.
 
 import torch
@@ -15,10 +15,8 @@ compute ='cpu'# 'cuda' for NVIDIA GPU, 'mps' for Mac GPU ('cuda:0', 'cuda:1'...)
 print("\n(1) Model   (Create a new model and save it as one file.)")
 print(  "(2) Train   (Train & test model on train.txt & test.txt.)")
 print(  "(3) Test    (See only testing on test.txt - no training.)")
-print(  "(4) Use     (Classify unlabeled cognize.txt - no spaces.)")
-o = int(input("\nOption: "))
+print(  "(4) Use     (Classify unlabeled cognize.txt - no spaces.)"); o = int(input("\nOption: "));
 
-# Needed to create/save/load model.
 model = nn.Sequential(); model.add_module('input',       nn.Linear(longest, width  )); model.add_module('relu1',     nn.ReLU());
 for a in range(depth):   model.add_module(f'hidden_{a}', nn.Linear(  width, width  )); model.add_module(f'relu_{a}', nn.ReLU());
 model.add_module                         ('output',      nn.Linear(  width, classes)); normalized = [0.0] * longest;
@@ -45,11 +43,11 @@ if o == 2: # Train______________________________________________________________
 if o == 3 or o == 2: # Test__________________________________________________________________________________________________________________________________________
 	model.load_state_dict(torch.load('Model.pth', map_location = compute))                                                             # Loads model from file.
 	with open('training-data/test.txt', 'rb') as f: total_testing_data_items = f.read().count(10)                                      # Number of items to test on.
-	model.eval(); misclassified = 0; print("\n", end = '');
-	in_stream = open('training-data/test.txt', 'r'); out_stream = open('results.txt', 'w');
+	misclassified = 0; off_by_summation = 0; model.eval(); print("\n", end = '');
+	in_stream = open('training-data/test.txt', 'r'); out_stream = open('results.txt', 'w'); out_xtra = open('results_extra.txt', 'w');
 	for a in range(total_testing_data_items):
 		print("Testing on test.txt line", (a + 1), "of", total_testing_data_items)
-		line = in_stream.readline().split(); expected_classification = int(line[0]);                                                   # Expected classification.
+		line = in_stream.readline().split(); expected_class = int(line[0]);                                                            # Expected classification.
 		normalized[:] = [0.0] * longest; length = len(line[1]);                                                                        # Data to be classified.
 		if length > longest: length = longest
 		for b in range(length):
@@ -57,11 +55,13 @@ if o == 3 or o == 2: # Test_____________________________________________________
 		input_data = torch.tensor(normalized).view(1, longest)
 		with torch.no_grad(): outputs = model(input_data); _, predictions = torch.max(outputs, 1);                                     # Uses model.
 		classification = predictions[0].item(); out_stream.write(f"{classification}\n");                                               # Saves classification.
-		if classification != expected_classification: misclassified += 1                                                               # Checks if misclassified.
-	in_stream.close(); out_stream.close();
-	print("\nMisclassifies", misclassified, "out of", total_testing_data_items, "(see results.txt)\n")
-	percent_correct = format((((total_testing_data_items - misclassified) / total_testing_data_items) * 100), ".15f")
-	print(percent_correct, end = '% correct.')
+		if classification != expected_class: misclassified += 1; off_by_summation += (abs(expected_class - classification));           # Checks if misclassified.
+		if classification == expected_class: out_xtra.write(f"{classification} (OK\n")
+		else: out_xtra.write(f"{classification} ({expected_class}  was the label - off by {abs(expected_class - classification)}\n")
+	in_stream.close(); out_stream.close(); out_xtra.close();
+	print("\n\n\n", format((((total_testing_data_items - misclassified) / total_testing_data_items) * 100), ".15f"), end = "% correct")
+	print(" (misclassifies", misclassified, "out of", total_testing_data_items, end = ")\n\n")
+	print(f"Off by {off_by_summation / misclassified} on average (see results_extra.txt)")
 
 if o == 4: # Use_____________________________________________________________________________________________________________________________________________________
 	model.load_state_dict(torch.load('Model.pth', map_location = compute))                                                             # Loads model from file.
